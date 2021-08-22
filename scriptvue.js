816c7864b4b337759ea01585ecb26cd786f10ef6
Giulio Braga <79613776+BragaVR@users.noreply.github.com>
2021-08-23 01:33:03 +0200

    Add files via upload

---
+++ scriptvue.js
@@ -0,0 +1,42 @@
+let products = [
+]; 
+
+//fare parte per salvarela quantità dell'ordine e fare la parte che permette di ordinare più cose assieme
+
+const MacchinaSelf = {
+	data() {
+		return {
+			count: 1
+			if (quantità) {
+				return true
+				quantità ++;
+
+			}else
+		}
+	} 
+
+};
+
+Vue.createApp(MacchinaSelf).mount('#app');
+
+
+
+
+
+
+
+
+
+
+ /*  
+
+<div v-for="quantità.quantità-ordine">
+        {{quantità.quantità}}
+        <div v-if="quantità-ordine">
+          <div v-for="quantità">
+            
+        
+        {{item += 1}}
+      </div>
+
+*/
